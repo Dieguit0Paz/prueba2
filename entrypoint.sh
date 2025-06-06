83ce04455ed085f909c0e1620a2980405706eac8
Dieguit0Paz <102425168+Dieguit0Paz@users.noreply.github.com>
2025-06-06 10:49:29 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
-
-# Crear archivo de configuración Odoo con variables del entorno
-cat > /opt/odoo/odoo.conf <<EOF
-[options]
-addons_path = addons
-admin_passwd = ${ADMIN_PASSWORD}
-db_host = ${DB_HOST}
-db_port = ${DB_PORT}
-db_user = ${DB_USER}
-db_password = ${DB_PASSWORD}
-log_level = info
-logfile = /opt/odoo/odoo.log
-EOF
-
-# Ejecutar Odoo
-exec /opt/odoo/venv/bin/python3 /opt/odoo/odoo-bin -c /opt/odoo/odoo.conf
+exec python odoo-bin \
+    -c odoo.conf \
+    --db_host=${DB_HOST} \
+    --db_port=${DB_PORT} \
+    --db_user=${DB_USER} \
+    --db_password=${DB_PASSWORD} \
+    --admin-password=${ADMIN_PASSWORD} \
+    "$@"
